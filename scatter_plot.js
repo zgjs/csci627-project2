ad94d2f4b3cd7c0fa5641335c424ab8b545130aa
zgjs <59938457+zgjs@users.noreply.github.com>
2024-04-24 02:05:29 -0500

    Update scatter plot to allow redrawing

--- scatter_plot.js
+++ scatter_plot.js
@@ -73,7 +73,78 @@ function draw() {
     .style("fill", "black")
     
 }
-        
+
+export const redraw = () => {
+    // Set domain mode
+    dl.cleanDomain();
+    
+    // let rotatedData = rotateData(data);
+    //console.log(dl.dataColumns);
+    
+    const x_axis_element = document.getElementById("x_axis")
+    const y_axis_element = document.getElementById("y_axis")
+    // const color_element = document.getElementById("color_attribute")
+    const x_axis_name = x_axis_element.value
+    const y_axis_name = y_axis_element.value
+    // const color_attribute_name = color_element.value
+    const x_axis_text = x_axis_element.options[x_axis_element.selectedIndex].text;
+    const y_axis_text = y_axis_element.options[y_axis_element.selectedIndex].text;
+    //console.log([x_axis_name, y_axis_name, x_axis_text, y_axis_text])
+    //return;
+    
+    // Create axis values
+    const x_axis = dl.dataColumns[x_axis_name].domain
+    .range([0, width]);
+    
+    const y_axis = dl.dataColumns[y_axis_name].domain
+    .range([height, 0]);
+
+    // const color_attribute = dl.dataColumns[color_attribute_name].domain
+    // .range(["blue", "green"]); //, "yellow", "orange", "red", "magenta"]);
+    
+    // clear scatterSvg
+    scatterSvg.selectAll("*").remove();
+
+    // Create axis elements
+    scatterSvg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x_axis))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .text(x_axis_text);
+    
+    scatterSvg.append("g")
+    .call(d3.axisLeft(y_axis))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -40)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .text(y_axis_text);
+    
+    scatterSvg.append("g")
+    .selectAll("dot")
+    .data(dl.data.filter(d => d[x_axis_name] != undefined && d[y_axis_name] != undefined))
+    .join("circle")
+    .attr("cx", d => x_axis(d[x_axis_name]))
+    .attr("cy", d => y_axis(d[y_axis_name]))
+    .attr("r", 1)
+    .style("fill", "black")
+}
+
+window.redraw=redraw;
         
 // Load data and then call main
-dl.loadJsonData("./data/processed/filtered_listings.json", draw);
+// dl.loadJsonData("./data/processed/filtered_listings.json", draw); // All interesting fields
+dl.loadJsonData("./data/processed/filtered_listings.json", draw); // required fields only
+
+document.getElementById("scatterplotform").addEventListener("submit", function(event){
+    event.preventDefault();
+    redraw();   
+});
